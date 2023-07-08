0e777dbc548317f659aeb5e6451e00fa62022998
jamikrut <jamikrut@student.wszib.edu.pl>
2023-07-08 12:16:00 +0200

    Dodawanie Kategorii

--- memoryapp/routes/categories.py
+++ memoryapp/routes/categories.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from memoryapp import app
 from memoryapp.repository import *
 
@@ -6,3 +6,11 @@ from memoryapp.repository import *
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify(get_categories())
+
+
+@app.route('/categories', methods=['POST'])
+def add_category():
+    r = request.json
+    category_name = r['name']
+
+    return jsonify(create_category(category_name)), 201
